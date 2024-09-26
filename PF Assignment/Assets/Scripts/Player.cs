fdea8c1e2ed602ce3bcfe498d2f5c3a479410385
Joris327 <joris.van.bellen@gmail.com>
2024-09-26 14:27:57 +0200

    fixed default jump bug
    introduced input bug

--- PF Assignment/Assets/Scripts/Player.cs
+++ PF Assignment/Assets/Scripts/Player.cs
@@ -15,14 +15,24 @@ public class Player : MonoBehaviour
         if (!rb) Debug.LogError("Rigidbody not found.");
     }
 
+    void Update()
+    {
+        InputMovement();
+    }
+
     void FixedUpdate()
+    {
+        DefaultMovement();
+    }
+
+    void DefaultMovement()
     {
         Vector3 toMove = new(velocity, rb.velocity.y, rb.velocity.z);
 
-        if (moveLeft) toMove = -toMove;
+        if (moveLeft) toMove.x = -toMove.x;
 
         rb.velocity = toMove;
-        Debug.Log(rb.velocity);
+        //Debug.Log(rb.velocity);
 
         if (Physics.Raycast(transform.position, new Vector3(rb.velocity.x, 0, 0), out RaycastHit hitInfo, 0.5f))
         {
@@ -31,9 +41,22 @@ public class Player : MonoBehaviour
                 if (hitInfo.transform.CompareTag("Structure"))
                 {
                     moveLeft = !moveLeft;
-                    rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+                    Jump();
                 }
             }
         }
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.anyKey) {
+            Debug.Log("jump");
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
     }
 }
